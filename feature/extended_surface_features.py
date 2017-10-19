49183cba9fffdcba95f96a0c7c5fb61c6482e7a1
Carl Ambroselli <git@carl-ambroselli.de>
2017-10-19 11:13:05 +0200

    Extract features out of tsagkias that were not part of the paper

---
+++ feature/extended_surface_features.py
@@ -0,0 +1,20 @@
+import numpy as np
+from feature.features import Features
+
+class ExtendedSurfaceFeatures(Features):
+  def __init__(self):
+    super().__init__('extended_surface_features')
+
+  def _extract_features(self, df):
+    features = []
+
+    hour = df['hour']
+    features.append(hour)
+
+    year = df['year']
+    features.append(year)
+
+    publish_datestring = df['publish_datestring']
+    features.append(publish_datestring)
+
+    return np.vstack(features).T
